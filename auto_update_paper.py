2b6cc5f1cddcf28723b91993d75c9d834cad8aec
CmdrNorthpaw <thetuxkid@gmail.com>
2020-07-01 12:12:45 +0100

    Environment variables are now loaded from a .env file

--- auto_update_paper.py
+++ auto_update_paper.py
@@ -2,9 +2,14 @@ import requests
 import pydactyl
 import pysftp
 from os import environ
+from dotenv import load_dotenv
+load_dotenv()
+
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
  
 sftp_url = environ.get('SFTP_URL')
-sftp_port = environ.get('SFTP_PORT')
+sftp_port = int(environ.get('SFTP_PORT'))
 sftp_user = environ.get('SFTP_USER')
 sftp_password = environ.get('SFTP_PASS')
 
@@ -12,6 +17,7 @@ paper_jarfile = environ.get('PAPER_JAR')
 
 panel_key = environ.get('PANEL_KEY')
 panel_url = environ.get('PANEL_URL')
+print(panel_url)
 
 panel = pydactyl.PterodactylClient(panel_url, panel_key)
 servers = panel.client.list_servers()
@@ -25,7 +31,7 @@ print('Downloading Paper JAR')
 request = requests.get('https://papermc.io/api/v1/paper/1.16.1/latest/download')
 open(paper_jarfile, 'wb').write(request.content)
 
-with pysftp.Connection(host=sftp_url, port=sftp_port, username=sftp_user, password=sftp_password) as sftp:
+with pysftp.Connection(host=sftp_url, port=sftp_port, username=sftp_user, password=sftp_password, cnopts=cnopts) as sftp:
     print('SFTP connection established')
     sftp.remove(paper_jarfile)
     print('Old JAR deleted, uploading new...')
@@ -33,5 +39,5 @@ with pysftp.Connection(host=sftp_url, port=sftp_port, username=sftp_user, passwo
     print('New JAR uploaded')
 
 print('Starting server back up...')
-panel.client.send_power_action('Start')
+panel.client.send_power_action(server_id, 'start')
 print('Server started')        
